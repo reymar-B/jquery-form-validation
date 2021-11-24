a74353ff74c332a128fbe87ea0726098c1881e62
Reymar Bongolto <ramyer88@gmail.com>
2021-11-24 15:52:14 +0800

    Commit for submission
    - Update banner
    - Create form input validation

--- main.js
+++ main.js
@@ -1,89 +1,110 @@
 jQuery(function(){
 
-
-
-
     // let regx = /([a-z])+\s?$/i;
     let regx = /^([a-z]+\s)*[a-z]+$/i;
     let regxEmail = /^([a-z0-9_\-\.])+\@([a-z0-9_\-\.])+\.([a-z]{2,4})$/i;
     let regxPhone = /([09])([\d]{2})\-([\d]{3})\-([\d]{4})$/;
+
+    $("#firstName, #lastName").on('blur', function(){
+            
+        let newValue;
+    
+        newValue = capitalizeWords($(this).val().toLowerCase());
+
+        $(this).val(newValue);
+
+        first = $(this).val();
+
+        function capitalizeWords(string){
+
+            return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+        };
+    
+    });
+
     $('#registrationForm').on('submit', function (event) {
-            event.preventDefault();
-            let firstName = $('#firstName').val();
-            let lastName = $('#lastName').val();
-            let email = $('#email').val();
-            let phoneNumber = $('#phoneNumber').val();
-            let errorCount = 0;
-            
+    
+        event.preventDefault();
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let email = $('#email').val();
+        let phoneNumber = $('#phoneNumber').val();
+        let errorCount = 0;
+        
 
-            if (firstName == " " || !regx.test(firstName)) {
+        if (firstName == " " || !regx.test(firstName)) {
 
-                $('#firstName + .error-msg').text('Pls specify a valid name');
-                $('#firstName').addClass('error');
-                errorCount++;
-            } else {
+            $('#firstName + .error-msg').text('Pls specify a valid name');
+    
+            $('#firstName').addClass('error');
+    
+            errorCount++;
+    
+        } else {
 
-                $('#firstName + .error-msg').html('');
-                $('#firstName').removeClass('error');
-            };
+            $('#firstName + .error-msg').html('');
+    
+            $('#firstName').removeClass('error');
+        };
 
-            if (lastName == " " || !regx.test(lastName)) {
+        if (lastName == " " || !regx.test(lastName)) {
 
-                $('#lastName + .error-msg').text('Pls specify a valid name');
-                $('#lastName').addClass('error');
-                errorCount++;
-            } else {
+            $('#lastName + .error-msg').text('Pls specify a valid name');
+    
+            $('#lastName').addClass('error');
+    
+            errorCount++;
+    
+        } else {
 
-                $('#lastName + .error-msg').html('');
-                $('#lastName').removeClass('error');
-            };
+            $('#lastName + .error-msg').html('');
+    
+            $('#lastName').removeClass('error');
+        };
 
-            if (email == " " || !regxEmail.test(email)) {
+        if (email == " " || !regxEmail.test(email)) {
 
-                $('#email + .error-msg').text('Pls specify a valid email');
-                $('#email').addClass('error');
-                errorCount++;
-            } else {
+            $('#email + .error-msg').text('Pls specify a valid email');
+    
+            $('#email').addClass('error');
+    
+            errorCount++;
+    
+        } else {
 
-                $('#email + .error-msg').html('');
-                $('#email').removeClass('error');
-            };
+            $('#email + .error-msg').html('');
+    
+            $('#email').removeClass('error');
+        };
 
-            if (phoneNumber == " " || !regxPhone.test(phoneNumber)) {
+        if (phoneNumber == " " || !regxPhone.test(phoneNumber)) {
 
-                $('#phoneNumber + .error-msg').text('Pls specify a valid Phone Number');
-                $('#phoneNumber').addClass('error');
-                errorCount++;
-            } else {
+            $('#phoneNumber + .error-msg').text('Pls specify a valid Phone Number');
 
-                $('#phoneNumber + .error-msg').html('');
-                $('#phoneNumber').removeClass('error');
-            };
+            $('#phoneNumber').addClass('error');
 
-            if (errorCount > 0) {
+            errorCount++;
 
-                errorCount = 0;
-            } else {
-                alert('Added successfully');
-                $('form').trigger('reset');
-            }
-
-        });
+        } else {
 
 
-        $("#firstName, #lastName").on('blur', function(){
-            
-            let newValue;
-            console.log($(this).val());
+            $('#phoneNumber + .error-msg').html('');
 
-            newValue = capitalizeWords($(this).val().toLowerCase());
+            $('#phoneNumber').removeClass('error');
+        };
 
-            $(this).val(newValue);
+        if (errorCount > 0) {
 
-            function capitalizeWords(string){
+            errorCount = 0;
 
-                return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-            };
-        });
+        } else {
+
+            alert('Added successfully');
+
+            $('form').trigger('reset');
+        }
+
+    });
+        
 })
 
